b336bbd618eff467ad1ca2079ae4ed2118235807
Jangey <jangey.lu@gmail.com>
2018-10-10 17:10:15 -0700

    4. User can use a pan gesture to drag faces from the image tray to the canvas

--- Canvas/CanvasViewController.swift
+++ Canvas/CanvasViewController.swift
@@ -10,8 +10,13 @@ import UIKit
 
 class CanvasViewController: UIViewController {
 
+    // element for trayPan
     @IBOutlet weak var trayView: UIView!
     var trayOriginalCenter: CGPoint!
+    
+    // element for faces
+    var newlyCreatedFace: UIImageView!
+    var newlyCreatedFaceOriginalCenter: CGPoint!
     
     // properties for tray up & tray down
     var trayDownOffset: CGFloat!
@@ -27,6 +32,7 @@ class CanvasViewController: UIViewController {
         
     }
 
+    // Pan for Tray
     @IBAction func didPanTray(_ sender: UIPanGestureRecognizer) {
         //let location = sender.location(in: view)
         let translation = sender.translation(in: view)
@@ -52,6 +58,39 @@ class CanvasViewController: UIViewController {
             }
         }
     }
+    
+    // Pan for happy face
+    @IBAction func didPanFace(_ sender: UIPanGestureRecognizer) {
+        
+        let translation = sender.translation(in: view)
+        
+        if sender.state == .began {
+            
+            //imageView now refers to the face that you panned on.
+            let imageView = sender.view as! UIImageView
+            
+            // Create a new image view that has the same image as the one you're currently panning.
+            newlyCreatedFace = UIImageView(image: imageView.image)
+            
+            //Add the new face to the main view.
+            view.addSubview(newlyCreatedFace)
+            
+            //Initialize the position of the new face.
+            newlyCreatedFace.center = imageView.center
+            
+            //Since the original face is in the tray, but the new face is in the main view, you have to offset the coordinates.
+            newlyCreatedFace.center.y += trayView.frame.origin.y
+            
+            newlyCreatedFaceOriginalCenter = newlyCreatedFace.center
+
+        } else if sender.state == .changed {
+            newlyCreatedFace.center = CGPoint(x: newlyCreatedFaceOriginalCenter.x + translation.x, y: newlyCreatedFaceOriginalCenter.y + translation.y)
+            
+        } else if sender.state == .ended {
+            
+        }
+    }
+    
     
 }
 
